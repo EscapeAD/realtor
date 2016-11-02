229fa6363729bceb305d465883f4251ed2b78f85
Adam Tse <mr.adam.tse@gmail.com>
2016-11-02 01:59:58 -0400

    color changes

--- src/js/components/sections/Listing.js
+++ src/js/components/sections/Listing.js
@@ -3,42 +3,34 @@ import React from 'react';
 export default class Listing extends React.Component {
   render() {
     return (
-      <div id="real-our-listing" data-section="Listing">
-        <div class="container">
-          <div class="row row-bottom-padded-sm">
-            <div class="col-md-12 section-heading text-center">
-              <h2 class="to-animate">FEATURED LISTINGS</h2>
-              <div class="row">
-                <div class="col-md-8 col-md-offset-2 to-animate">
-                  <h3>Current Featured Listings</h3>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-4">
-              <div class="box to-animate">
-                <h3>100% free</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-              </div>
+      <div id="real-press" data-section="press">
+    		<div class="container">
+    			<div class="row">
+    				<div class="col-md-12 section-heading text-center">
+    					<h2 class="single-animate animate-press-1">Featured Listings</h2>
+    					<div class="row">
+    						<div class="col-md-8 col-md-offset-2 subtext single-animate animate-press-2">
+    							<h3>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</h3>
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="row">
+    				<div class="col-md-6">
+    					<div class="real-press-item to-animate">
+    						<div class="real-press-img">
+    						</div>
+    						<div class="real-press-text">
+    							<h3 class="h2 real-press-title">Simplicity <span class="real-border"></span></h3>
+    							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eius quos similique suscipit dolorem cumque vitae qui molestias illo accusantium...</p>
+    							<p><a href="#" class="btn btn-primary btn-sm">Learn more</a></p>
+    						</div>
+    					</div>
+    				</div>
 
-            </div>
-            <div class="col-md-4">
-              <div class="box to-animate">
-                <h3>Fully responsive</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              </div>
-
-            </div>
-            <div class="col-md-4">
-              <div class="box to-animate">
-                <h3>Retina-ready</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    			</div>
+    		</div>
+    	</div>
     );
   }
 }
