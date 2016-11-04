d5941c33df01ac3bbd73f2a11dbde2c377c76b46
Adam Tse <mr.adam.tse@gmail.com>
2016-11-04 19:36:52 -0400

    +added ejs

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express'),
       app     = express();
 
+app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
 
 app.get("/", (req,res) => {
-    res.render(index.html);
+    res.render(index);
 });
 
 app.listen(process.env.PORT || 3000, () => {
